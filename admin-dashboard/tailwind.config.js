0a0e00f2a8b692eef681cf58d150b85089e8d455
haihm191 <119120119+2hm1901@users.noreply.github.com>
2025-04-08 12:29:27 +0700

    Completed Docker and Nginx

---
+++ admin-dashboard/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}", // Quét tất cả file .ts và .tsx trong src
+  ],
+  theme: {
+    extend: {}, // Tùy chỉnh theme nếu cần (màu sắc, font, v.v.)
+  },
+  plugins: [],
+};
